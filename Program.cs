b4c64080b4a638484df01297a14e5aaf5c99ab63
Vladimir <user@MacBook-Air-user.local>
2022-10-03 22:12:09 +0700

    домашнее задание 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, User! Введите первое число");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine()!);
+if (num1>num2)
+{
+   Console.WriteLine($"{num1} максимальное"); 
+   Console.WriteLine($"{num2} минимальное");
+}
+else
+{
+   Console.WriteLine($"{num2} максимальное");  
+   Console.WriteLine($"{num1} минимальное");
+}
